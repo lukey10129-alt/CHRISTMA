2cec29be0059d63a4be58883a0fcd67c38a22c4d
luke hall <76048944+lukey10129-alt@users.noreply.github.com>
2021-12-06 20:56:33 +0000

    Add files via upload

---
+++ CHRISTMA.py
@@ -0,0 +1,28 @@
+import time
+def main():
+    print("                *")
+    print("                *")
+    print("               ***")
+    print("              *****")
+    print("             *******")
+    print("            *********")
+    print("          *************                A")
+    print("             *******")
+    print("           ***********                VERY")
+    print("         ***************")
+    print("       *******************            HAPPY")
+    print("           ***********")
+    print("         ***************            CHRISTMAS")
+    print("       *******************")
+    print("     ***********************         AND MY")
+    print("         ***************")
+    print("       *******************         BEST WISHES")
+    print("     ***********************")
+    print("   ***************************     FOR THE NEXT")
+    print("             ******")
+    print("             ******                    YEAR")
+    print("             ******")
+    time.sleep(100)
+main()
+
+
